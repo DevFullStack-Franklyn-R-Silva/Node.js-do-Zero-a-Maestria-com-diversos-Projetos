01975696794e8eb47a6e01e10a49feea2aa2a967
hadesfranklyn <franklyn1998@live.com>
2023-08-11 16:40:54 -0300

    Inicialização do sistema

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -5,4 +5,28 @@ const chalk = require("chalk");
 //modulos internos
 const fs = require("fs");
 
-console.log("Iniciamos o Accounts");
+operation();
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "O que você deseja fazer?",
+                choices: [
+                    "Criar conta",
+                    "Consultar Saldo",
+                    "Depositar",
+                    "Sacar",
+                    "Sair",
+                ],
+            },
+        ])
+        .then((answer) => {
+            const action = answer["action"];
+
+            console.log(action);
+        })
+        .catch((err) => console.log(err));
+}
