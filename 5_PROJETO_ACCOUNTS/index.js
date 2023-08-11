b422b1a65cd08b0a187e1fb98fb1c8b8c14070b9
hadesfranklyn <franklyn1998@live.com>
2023-08-11 16:59:17 -0300

    Finalizando criação de conta

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -29,6 +29,7 @@ function operation() {
 
             if (action === "Criar conta") {
                 createAccount();
+                buildAccount();
             }
         })
         .catch((err) => console.log(err));
@@ -37,5 +38,45 @@ function operation() {
 // create an account
 function createAccount() {
     console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"));
-    console.log(chalk.green("Defina as opções da sua conta a seguir"));
+    console.log(chalk.green("Defina as opções da sua conta a seguir: "));
+}
+
+//
+function buildAccount() {
+    inquirer
+        .prompt([
+            {
+                name: "accountName",
+                message: "Digite um nome para a sua conta: ",
+            },
+        ])
+        .then((answer) => {
+            const accountName = answer["accountName"];
+
+            console.info(accountName);
+
+            if (!fs.existsSync("accounts")) {
+                fs.mkdirSync("accounts");
+            }
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(
+                    chalk.bgRed.black(
+                        "Essa conta já existe, escolha outro nome!"
+                    )
+                );
+                buildAccount();
+                return;
+            }
+
+            fs.writeFileSync(
+                `accounts/${accountName}.json`,
+                '{"balance": 0}',
+                function (err) {
+                    console.log(err);
+                }
+            );
+            console.log(chalk.green("Parabéns sua conta foi criada!"))
+            operation();
+        })
+        .catch((err) => console.log(err));
 }
