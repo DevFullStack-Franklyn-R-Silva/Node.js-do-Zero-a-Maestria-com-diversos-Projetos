d7467cded193228037fa686d7b77b030bc3f4718
hadesfranklyn <franklyn1998@live.com>
2023-08-14 10:56:15 -0300

    Finalização do saque

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -227,8 +227,37 @@ function withdraw() {
                 .then((answer) => {
                     const amount = answer["amount"];
 
-                    console.log(amount);
+                    removeAmount(accountName, amount);
                 }).catch;
         })
         .catch((err) => console.log(err));
+}
+function removeAmount(accountName, amount) {
+    const accountData = getAccount(accountName);
+
+    if (!amount) {
+        console.log(
+            chalk.bgRed.black("Ocorreu um erro, tente novamente mais tarde!")
+        );
+        return withdraw();
+    }
+
+    if (accountData.balance < amount) {
+        console.log(chalk.bgRed.black("Valor indisponível!"));
+        return withdraw();
+    }
+
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err);
+        }
+    );
+
+    console.log(
+        chalk.green(`Foi realizado um saque de R$${amount} da sua conta!`)
+    );
 }
