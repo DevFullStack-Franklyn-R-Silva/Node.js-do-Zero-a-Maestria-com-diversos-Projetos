16cbe30167f99eda2036dae87c65ed0a9a6a4cd6
hadesfranklyn <franklyn1998@live.com>
2023-08-18 14:26:47 -0300

    Interface Gráfica com Vue JS #02

--- 00_PROJETOS_CURSO_WEB_MODERNO_COMPLETO_COM_JS_2022/1_palavras_mais_usadas_em_ingles/most-used-words/src/components/HomePage.vue
+++ 00_PROJETOS_CURSO_WEB_MODERNO_COMPLETO_COM_JS_2022/1_palavras_mais_usadas_em_ingles/most-used-words/src/components/HomePage.vue
@@ -1,17 +1,30 @@
 <template>
-    <div class="pills">
-        <PillComponent
-            v-for="item in groupedWords"
-            :word="item.word"
-            :amount="item.amount"
-            :key="item.word"
-        />
+    <div class="home">
+        <v-file-input
+            label="Selecione as legendas"
+            prepend-icon="mdi-message-text"
+            append-icon="mdi-send"
+            outlined
+            multiple
+            v-model="files"
+            @click:append="processSubtitles"
+        >
+        </v-file-input>
+        <div class="pills">
+            <PillComponent
+                v-for="item in groupedWords"
+                :word="item.word"
+                :amount="item.amount"
+                :key="item.word"
+            />
+        </div>
     </div>
 </template>
 
 <script>
 import PillComponent from "./PillComponent.vue";
 export default {
+    files: [],
     components: {
         PillComponent,
     },
@@ -24,11 +37,22 @@ export default {
             ],
         };
     },
+    methods: {
+        processSubtitles() {
+            console.log(this.files);
+        },
+    },
 };
 </script>
 
 <style scoped>
+
+.home{
+    margin: 20px;
+}
 .pills {
     display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 }
 </style>
